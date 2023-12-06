ec3fd1871efa5339064e3a3e9f2af184a17d9bdc
Aleksander <133510647+sanchez005909@users.noreply.github.com>
2023-12-06 19:02:57 +0300

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.static import static
 from django.urls import path
 from django.conf import settings
-
+from django.views.decorators.cache import cache_page
 from blog.apps import BlogConfig
 from blog.views import BlogListView, BlogDetailView
 from newsletter.apps import NewsletterConfig
@@ -9,6 +9,6 @@ from newsletter.apps import NewsletterConfig
 app_name = BlogConfig.name
 
 urlpatterns = [
-                  path('blog_page/', BlogListView.as_view(), name='blog_page'),
+                  path('blog_page/', cache_page(60)(BlogListView.as_view()), name='blog_page'),
                   path('blog_detail/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
